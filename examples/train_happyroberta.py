5772482de1704e3e607a0522844c954afb9a426e
Rahul Khanna <rahulkhanna@rahuls-mbp-2.attlocal.net>
2020-05-15 21:42:57 -0700

    making the script a bit more flexible

--- examples/train_happyroberta.py
+++ examples/train_happyroberta.py
@@ -1,16 +1,20 @@
 import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="3"
+os.environ["CUDA_VISIBLE_DEVICES"]="6"
 import sys
 sys.path.append('../')
 from happytransformer import HappyROBERTA
+if len(sys.argv):
+    model_name = sys.argv[0]
+else:
+    model_name = "roberta-base"
 
-roberta = HappyROBERTA()
+roberta = HappyROBERTA(model_name)
 
 word_prediction_args = {
 "batch_size": 1,
 
-"epochs": 5,
+"epochs": 20,
 
 "lr": 1e-4,
 
@@ -24,6 +28,6 @@ train_path = '../../data/finetune_data/train_sentences.txt'
 eval_path = '../../data/finetune_data/eval_sentences.txt'
 train_masked_path = '../../data/finetune_data/train_m_sentences.txt'
 eval_masked_path = '../../data/finetune_data/eval_m_sentences.txt'
-output_dir = '../../data/finetune_data'
+output_dir = '../../data/finetune_data/{}'.format(model_name)
 
 roberta.train_mwp(train_path, eval_path, train_masked_path, eval_masked_path, output_dir)
